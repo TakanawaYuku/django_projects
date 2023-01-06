0626677c7064035f89af553c0cf2aecd5d8088c8
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-06 16:52:40 +0300

    add class author

--- library_site_local/locallibrary/catalog/models.py
+++ library_site_local/locallibrary/catalog/models.py
@@ -89,9 +89,33 @@ class BookInstance(models.Model):
     class Meta:
         ordering = ['due_back']
 
-
     def __str__(self):
         """
             Строка для представления объекта Model
         """
         return '%s (%s)' % (self.id, self.book.title)
+
+
+class Author(models.Model):
+    """
+        Модель, представляющая автора.
+    """
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    def get_absolute_url(self):
+        """
+            Возвращает URL-адрес для доступа к конкретному экземпляру автора.
+        """
+
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """
+            Строка для представления объекта Model
+        """
+
+        return '%s, %s' % (self.last_name, self.first_name)
