a5d2c05f0cd8d4f3e39935aa4fd813132bdd7930
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-06 15:49:24 +0300

    create models site

--- library_site_local/locallibrary/catalog/models.py
+++ library_site_local/locallibrary/catalog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
+class MyModelName(models.Model):
+
+    my_field_name = models.CharField(max_length=20,
+                                     help_text='Введите полевую документацию')
+
+    class Meta:
+        ordering = ['-my_field_name']
+        # ordering = ['title', '-pubdate']
+        # verbose_name = 'Better name'
