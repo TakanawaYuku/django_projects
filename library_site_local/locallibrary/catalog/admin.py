df340820193d3148da3efd86b9deddafeaaa9e91
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-07 15:43:24 +0300

    registreted models

--- library_site_local/locallibrary/catalog/admin.py
+++ library_site_local/locallibrary/catalog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Author, Book, BookInstance, Language, Genre
 
 # Register your models here.
+admin.register(Book)
+admin.register(Author)
+admin.register(Genre)
+admin.register(BookInstance)
+admin.register(Language)
