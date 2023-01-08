1e60f47fe19b3f8cbdcbbd352d1a2a5ff59c18e6
TakanawaYuku <yukutakanawa@gmail.com>
2023-01-08 15:27:40 +0300

    changes path

--- library_site_local/locallibrary/locallibrary/urls.py
+++ library_site_local/locallibrary/locallibrary/urls.py
@@ -21,8 +21,11 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+urlpatterns = + [
+    path('catalog/', include('catalog.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
